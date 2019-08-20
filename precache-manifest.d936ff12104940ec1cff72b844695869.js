5893cc6b1f1e5ede20534c3c527158521bbe0528
supertradersind <supertradersind@gmail.com>
2019-08-20 10:17:37 +0530

    Updates

---
+++ precache-manifest.d936ff12104940ec1cff72b844695869.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f46a9c17bde5f80e72b99fb39bc11e",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "a8b8f376041c8eba100c",
+    "url": "/react-gh-pages/static/css/main.e50d795d.chunk.css"
+  },
+  {
+    "revision": "a85339011f3f934200b0",
+    "url": "/react-gh-pages/static/js/2.3b249966.chunk.js"
+  },
+  {
+    "revision": "a8b8f376041c8eba100c",
+    "url": "/react-gh-pages/static/js/main.3ee871e0.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/react-gh-pages/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/react-gh-pages/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/react-gh-pages/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/react-gh-pages/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "c3a570d3df450f92f6ead29e34719f5b",
+    "url": "/react-gh-pages/static/media/bg8.c3a570d3.jpeg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/react-gh-pages/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/react-gh-pages/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/react-gh-pages/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/react-gh-pages/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/react-gh-pages/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/react-gh-pages/static/media/sign.cf8b686b.jpg"
+  }
+]);
